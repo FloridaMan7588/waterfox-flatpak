1559df876009833b47849f7ba95268880c1a3ea9
FloridaMan7588 <cayden.haun75@gmail.com>
2022-04-23 16:58:29 -0500

    Update setup_runtime.sh
    Fix potential problems with CircleCI

--- setup_runtime.sh
+++ setup_runtime.sh
@@ -3,7 +3,7 @@ set -ux
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install flathub org.gnome.Platform//42 org.gnome.Sdk//42
+flatpak install -y flathub org.gnome.Platform//42 org.gnome.Sdk//42
 #flatpak install flathub org.gnome.Platform//3.26 org.gnome.Sdk//3.26
-flatpak install flathub org.freedesktop.Sdk.Extension.rust-stable//1.6
-flatpak install flathub org.freedesktop.Platform//21.08 org.freedesktop.Sdk//21.08
+flatpak install -y flathub org.freedesktop.Sdk.Extension.rust-stable//1.6
+flatpak install -y flathub org.freedesktop.Platform//21.08 org.freedesktop.Sdk//21.08
