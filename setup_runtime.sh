aca5f96a4ce67bb914248bc233a6d005cfaa6765
FloridaMan7588 <cayden.haun75@gmail.com>
2022-06-10 12:14:17 -0500

    Fix Waterfox G4 compilation
    After some work and a lot of figuring out how flatpak works, I got Waterfox G4 to successfully compile. Now I'm just working out any minor kinks in the system.

--- setup_runtime.sh
+++ setup_runtime.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -ux
+source /etc/os-release
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install -y flathub org.gnome.Platform//42 org.gnome.Sdk//42
-#flatpak install flathub org.gnome.Platform//3.26 org.gnome.Sdk//3.26
 flatpak install -y flathub org.freedesktop.Sdk.Extension.rust-stable//1.6
-flatpak install -y flathub org.freedesktop.Platform//21.08 org.freedesktop.Sdk//21.08
+flatpak install -y flathub org.freedesktop.Sdk.Extension.llvm13//21.08
+flatpak install -y flathub org.freedesktop.Sdk.Extension.node18//21.08
+#flatpak install -y flathub org.freedesktop.Platform//21.08 org.freedesktop.Sdk//21.08
