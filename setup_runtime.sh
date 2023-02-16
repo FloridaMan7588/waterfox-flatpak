0012bf9cc9c55ece3e5d911bf7e09c3811b3806d
FloridaMan7588 <floridaman7588@gmail.com>
2023-02-15 18:34:57 -0600

    Add G5 files
    Refactors a lot of code and pushes update for G5

--- setup_runtime.sh
+++ setup_runtime.sh
@@ -4,8 +4,8 @@ source /etc/os-release
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install -y flathub org.gnome.Platform//42 org.gnome.Sdk//42
-flatpak install -y flathub org.freedesktop.Sdk.Extension.rust-stable//1.6
-flatpak install -y flathub org.freedesktop.Sdk.Extension.llvm13//21.08
-flatpak install -y flathub org.freedesktop.Sdk.Extension.node18//21.08
+flatpak install -y flathub org.freedesktop.Platform//22.08 org.freedesktop.Sdk//22.08
+flatpak install -y flathub org.freedesktop.Sdk.Extension.rust-stable//22.08
+flatpak install -y flathub org.freedesktop.Sdk.Extension.llvm14//22.08
+flatpak install -y flathub org.freedesktop.Sdk.Extension.node18//22.08
 #flatpak install -y flathub org.freedesktop.Platform//21.08 org.freedesktop.Sdk//21.08
